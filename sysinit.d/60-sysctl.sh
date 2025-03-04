18e06b0af03d6a41725636c03465c594e74cc3f5
neeshy <neeshy@tfwno.gf>
2025-03-04 01:34:38 -0500

    Use sysctl --system

--- sysinit.d/60-sysctl.sh
+++ sysinit.d/60-sysctl.sh
@@ -1,6 +1,4 @@
 if cmd sysctl; then
     msg 'Loading sysctl(8) settings...'
-    for f in /usr/lib/sysctl.d/*.conf /etc/sysctl.d/*.conf /etc/sysctl.conf; do
-        [ -e "$f" ] && sysctl -p -- "$f"
-    done
+    sysctl --system
 fi
